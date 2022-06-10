62fdb28b1e74484e5779280fe9c0258ca5a8e296
efendykn27 <efendikn27@gmail.com>
2022-06-10 21:46:48 +0700

    script

---
+++ app.py
@@ -0,0 +1,241 @@
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense,Conv2D,MaxPool2D,Dropout,BatchNormalization,Flatten,Activation
+from keras.preprocessing import image 
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+from keras.utils.vis_utils import plot_model
+import pickle
+from flask import Flask, jsonify,request,flash,redirect,render_template, session,url_for
+#from flask_session import Session
+from itsdangerous import json
+from werkzeug.utils import secure_filename
+import os
+from flask_cors import CORS
+from flask_restful import Resource, Api
+import pymongo
+import re
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+#sess = Session()
+UPLOAD_FOLDER = 'foto_burung'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.secret_key = "bigtuing"
+#SECRET_KEY = 'xxxxxxxxx'
+#app.config['SESSION_TYPE'] = 'filesystem'
+MONGO_ADDR = 'mongodb://localhost:27017'
+MONGO_DB = "db_mibird"
+
+conn = pymongo.MongoClient(MONGO_ADDR)
+db = conn[MONGO_DB]
+
+api = Api(app)
+#CORS(app)
+
+
+from tensorflow.keras.models import load_model
+MODEL_PATH = 'model12lokal.h5'
+model = load_model(MODEL_PATH,compile=False)
+
+pickle_inn = open('num_12class_bird.pkl','rb')
+num_classes_bird = pickle.load(pickle_inn)
+
+def allowed_file(filename):     
+  return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+class index(Resource):
+  def post(self):
+
+    if 'image' not in request.files:
+      flash('No file part')
+      return jsonify({
+            "pesan":"tidak ada form image"
+          })
+    file = request.files['image']
+    if file.filename == '':
+      return jsonify({
+            "pesan":"tidak ada file image yang dipilih"
+          })
+    if file and allowed_file(file.filename):
+      # path_del = r"foto_burung\\"
+      # for file_name in os.listdir(path_del):
+      #   # construct full file path
+      #   file_del = path_del + file_name
+      #   if os.path.isfile(file_del):
+      #       print('Deleting file:', file_del)
+      #       os.remove(file_del)
+      #       print("file "+file_del+" telah terhapus")
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      path=("foto_burung/"+filename)
+
+      #def predict(dir):
+      img=image.load_img(path,target_size=(224,224))
+      img1=image.img_to_array(img)
+      img1=img1/255
+      img1=np.expand_dims(img1,[0])
+      plt.imshow(img)
+      predict=model.predict(img1)
+      classes=np.argmax(predict,axis=1)
+      for key,values in num_classes_bird.items():
+          if classes==values:
+            accuracy = float(round(np.max(model.predict(img1))*100,2))
+            info = db['data_burung'].find_one({'nama': str(key)})
+
+            if accuracy >35:
+              print("The predicted image of the bird is: "+str(key)+" with a probability of "+str(accuracy)+"%")
+
+              db.riwayat.insert_one({'nama_file': filename, 'path': path, 'prediksi':str(key), 'akurasi':accuracy})
+            
+              return jsonify({
+                "Nama_Burung":str(key),
+                "Accuracy":str(accuracy)+"%",
+                "Nama_Ilmiah": info['nama_ilmiah'],
+                "Spesies" : info['spesies'],
+                "Makanan" : info['makanan'],
+                "Status" :  info['status']         
+                
+              })
+            else :
+              print("The predicted image of the bird is: "+str(key)+" with a probability of "+str(accuracy)+"%")
+              return jsonify({
+                "Message":str("Jenis Burung belum tersedia "),
+                "Accuracy":str(accuracy)+"%"               
+                
+              })
+      
+    else:
+      return jsonify({
+        "Message":"bukan file image"
+      })
+
+@app.route('/admin')
+def admin():
+    return render_template("login.html")
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password'] # .encode('utf-8')
+        user = db['admin'].find_one({'username': str(username)})
+        print(user)
+
+        if user is not None and len(user) > 0:
+            if password == user['password']:
+                
+                session['username'] = user['username']
+                return redirect(url_for('dataBurung'))
+            else:
+                return redirect(url_for('login'))
+        else:
+            return redirect(url_for('login'))
+    else:
+        return render_template('login.html')
+    
+    return render_template('dashboard.html')
+#menampilkan  daftar tamu
+@app.route('/dataBurung')
+def dataBurung():
+    data = db['data_burung'].find({})
+    print(data)
+    return render_template('dataBurung.html',dataBurung  = data)
+
+@app.route('/riwayat')
+def riwayat():
+    dataRiwayat = db['riwayat'].find({})
+    print(dataRiwayat)
+    return render_template('riwayat.html',riwayat  = dataRiwayat)
+
+@app.route('/tambahData')
+def tambahData():
+
+    return render_template('tambahData.html')
+
+#roses memasukan data Burung ke database
+@app.route('/daftarBurung', methods=["POST"])
+def daftarBurung():
+    if request.method == "POST":
+        nm_burung = request.form['nm_burung']
+        nm_ilm = request.form['nm_ilmiah']
+        spesies = request.form['spesies']
+        makanan = request.form['makanan']
+        status = request.form['status']
+        if not re.match(r'[A-Za-z]+', nm_burung):
+            flash("Nama harus pakai huruf Dong!")
+        
+        else:
+            db.data_burung.insert_one({'nama': nm_burung, 'nama_ilmiah': nm_ilm, 'spesies':spesies, 'makanan':makanan, 'status':status})
+            flash('Data Burung berhasil ditambah')
+            return redirect(url_for('dataBurung'))
+
+    return render_template("tambahData.html")
+
+@app.route('/editBurung/<nama>', methods = ['POST', 'GET'])
+def editBurung(nama):
+  
+    data = db['data_burung'].find_one({'nama': nama})
+    print(data)
+    return render_template('editBurung.html', editBurung = data)
+
+#melakukan roses edit data
+@app.route('/updateBurung/<nama>', methods=['POST'])
+def updatBurung(nama):
+    if request.method == 'POST':
+        nm_burung = request.form['nm_burung']
+        nm_ilm = request.form['nm_ilmiah']
+        spesies = request.form['spesies']
+        makanan = request.form['makanan']
+        status = request.form['status']
+        if not re.match(r'[A-Za-z]+', nama):
+            flash("Nama harus pakai huruf Dong!")
+        else:
+          db.data_burung.update_one({'nama': nama}, 
+          {"$set": {
+            'nama': nm_burung, 
+            'nama_ilmiah': nm_ilm, 
+            'spesies':spesies, 
+            'makanan':makanan, 
+            'status':status
+            }
+            })
+
+          flash('Data Burung berhasil diupdate')
+          return render_template("popUpEdit.html")
+
+    return render_template("dataBurung.html")
+
+#menghaus daftar Burung
+@app.route('/hapusBurung/<nama>', methods = ['POST','GET'])
+def hapusBurung(nama):
+  
+    db.data_burung.delete_one({'nama': nama})
+    flash('Burung Berhasil Dihapus!')
+    return redirect(url_for('dataBurung'))
+
+@app.route('/hapusRiwayat/<nama_file>', methods = ['POST','GET'])
+def hapusRiwayat(nama_file):
+  
+    db.riwayat.delete_one({'nama_file': nama_file})
+    flash('Riwayat Berhasil Dihapus!')
+    return redirect(url_for('riwayat'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+
+api.add_resource(index, "/api/image", methods=["POST"])
+
+if __name__ == '__main__':
+  
+
+  app.run()
+  #app.run(debug = True, port=5000, host='0.0.0.0')
+
